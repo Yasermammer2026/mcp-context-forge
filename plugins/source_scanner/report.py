b74819b89a90327c0eb68a20cd766334ef1ac058
Yanko Yanchev <yanchevy@tcd.ie>
2026-02-18 12:15:52 +0000

    Update file report.py

--- plugins/source_scanner/report.py
+++ plugins/source_scanner/report.py
@@ -1,57 +1,64 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """Location: ./plugins/source_scanner/report.py
-Copyright 2025
+Copyright 2026
 SPDX-License-Identifier: Apache-2.0
 Author: Yanko
 
-Generates a report from the scanner json files
+Generates a report from the scanner json findings
 """
-#from plugins.source_scanner.parsing.normalizer import Normalizer
-import json
-from typing import Any
+# from plugins.source_scanner.parsing.normalizer import Normalizer
+
+# Standard
+from typing import List
+
+# First-Party
+from plugins.source_scanner.types import Finding
 
 
-class report:
-    def __init__(self, report_path: str, filename: str, scanner: str):
-        self.report_path = report_path
-        self.filename = filename
-        self.scanner = scanner
-        self.data: list[Any] = []
-        self.load()
+class Report:
+    """Generates a report from the scanner's findings"""
 
-    def load(self):
-        with open(self.report_path, 'r') as file:
-            self.data = json.load(file)
-        return self.data
-    
-    
+    def __init__(self, findings: List[Finding]):
+        """initialize the report
+
+        Args:
+            findings: List of findings from the scanner
+        """
+
+        self.findings = findings
+
     def summary(self) -> dict[str, int]:
-        levels = {"HIGH" : 0, "MEDIUM" : 0, "LOW" : 0}
+        """gives a summary of the findings
 
-        for file in self.data:
-            levels[file["severity"]] += 1
+        Returns:
+            A dictionary with a summary of the findings"""
 
+        levels = {"ERROR": 0, "WARNING": 0, "INFO": 0}
+
+        for finding in self.findings:
+            levels[finding["severity"]] += 1
 
         summary = {
-            "total_files" : len(self.data),
-            "total_issues" : (levels["HIGH"] + levels["MEDIUM"] + levels["LOW"]),
-            "HIGH" : levels["HIGH"],
-            "MEDIUM" : levels["MEDIUM"],
-            "LOW" : levels["LOW"]
+            "total_findings": len(self.findings),
+            "total_issues": (levels["ERROR"] + levels["WARNING"] + levels["INFO"]),
+            "ERROR": levels["ERROR"],
+            "WARNING": levels["WARNING"],
+            "INFO": levels["INFO"],
         }
         return summary
-    
+
     def ordered(self) -> dict[str, dict[str, int]]:
-        ordered: dict[str, dict[str, int]] = { "HIGH" : {}, "MEDIUM" : {}, "LOW" : {} }
+        """gives an rdered list of the findings
 
-        for file in self.data:
-            if file["issue"] not in ordered[file["severity"]]:
-                ordered[file["severity"]][file["issue"]] = 1
+        Returns:
+            A dictionary with 3 dictionaries named after the severity levels, each containing issues of that severity and their counts"""
+
+        ordered: dict[str, dict[str, int]] = {"ERROR": {}, "WARNING": {}, "INFO": {}}
+
+        for finding in self.findings:
+            if finding["issue"] not in ordered[finding["severity"]]:
+                ordered[finding["severity"]][finding["issue"]] = 1
             else:
-                ordered[file["severity"]][file["issue"]] += 1
+                ordered[finding["severity"]][finding["issue"]] += 1
         return ordered
-    
-    
-        
-
